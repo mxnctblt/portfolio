d0eefe0c33e46f2e67d151cedca8098920c19ab6
mxnc <thibaultmaxence44@gmail.com>
2023-06-07 09:32:42 +0200

    adding can't post if no img

--- interlude/views.py
+++ interlude/views.py
@@ -21,14 +21,18 @@ def feed(request):
 @login_required(login_url='login')
 def upload(request):
     if request.method == 'POST':
-        user = request.user.username
-        image = request.FILES.get('image_upload')
-        caption = request.POST['caption']
+        if request.FILES.get('image_upload') == None:
+                messages.info(request, 'Missing an image')
+                return redirect('/')
+        else:
+            user = request.user.username
+            image = request.FILES.get('image_upload')
+            caption = request.POST['caption']
 
-        new_post = Post.objects.create(user=user, image=image, caption=caption)
-        new_post.save()
+            new_post = Post.objects.create(user=user, image=image, caption=caption)
+            new_post.save()
 
-        return redirect('/')
+            return redirect('/')
     else:
         return redirect('/')
 
