959c099e7b9f9ea805e8a1f2dd0a84704e51dd06
mxnc <thibaultmaxence44@gmail.com>
2023-06-18 22:16:13 +0200

    cleaning

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('posts/', views.posts),
-    path('profiles/', views.profiles),
-    path('likes/', views.likes),
-    path('followers/', views.followers),
-]
