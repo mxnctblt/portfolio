e68782400e8df4a952535e53fb9a13d147e7705d
mxnc <thibaultmaxence44@gmail.com>
2023-06-23 03:10:56 +0200

    new branch + search view updated

--- interlude/views.py
+++ interlude/views.py
@@ -285,25 +285,32 @@ def delete_comment_profile(request):
 
 @login_required(login_url='login')
 def search(request):
+    referer = request.META.get('HTTP_REFERER')
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
     if request.method == 'POST':
-        username = request.POST['username']
-        username_object = User.objects.filter(username__icontains=username)
+        query = request.POST['query']
 
-        username_profile = []
-        username_profile_list = []
+        if query.startswith('#'):
+            posts = Post.objects.filter(caption__icontains=query)
+            return render(request, 'hashtag_search.html', {'user_profile': user_profile, 'posts': posts, 'hashtag': query})
+        else:
+            username_object = User.objects.filter(username__icontains=query)
 
-        for users in username_object:
-            username_profile.append(users.id)
+            username_profile = []
+            username_profile_list = []
 
-        for ids in username_profile:
-            profile_lists = Profile.objects.filter(id_user=ids)
-            username_profile_list.append(profile_lists)
-        
-        username_profile_list = list(chain(*username_profile_list))
-    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
+            for users in username_object:
+                username_profile.append(users.id)
+
+            for ids in username_profile:
+                profile_lists = Profile.objects.filter(id_user=ids)
+                username_profile_list.append(profile_lists)
+            
+            username_profile_list = list(chain(*username_profile_list))
+        return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
+    return redirect(referer)
 
 def embed_url(video_url):
         regex = r"(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)"
